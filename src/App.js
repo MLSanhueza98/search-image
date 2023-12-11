5d95a5bdb646380ae284326afc4702f145c127e5
Mario Labbé <mario.ignacio19@gmail.com>
2023-12-10 23:46:39 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import './header.css'
+import './container.css'
+import './article.css'
 
-function App() {
+
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  console.log({photos})
+  const open = url => {
+    window.open(url)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues = {{ search: '' }}
+          onSubmit = { async values => {
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+            {
+              headers: {
+                'Authorization': 'Client-ID VfIIny6eJit5HcqAPxqSwoEldbCWBee6KpOlYs49-UM',
+              }
+            })
+            const data = await response.json()
+            setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+
+        </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo =>
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.raw} />
+              <p> {photo.alt_description.charAt(0).toUpperCase() + photo.alt_description.slice(1).toLowerCase()} </p>
+            </article>
+            )}
+        </div>
+      </div>
+      
     </div>
-  );
+  )
 }
 
 export default App;
